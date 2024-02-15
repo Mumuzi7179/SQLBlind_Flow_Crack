a2172b2b7c0484adb8567882fb518527be04411b
FW_Mumuzi <74121593+Mumuzi7179@users.noreply.github.com>
2024-02-15 14:20:10 +0800

    Update crack.py

--- crack.py
+++ crack.py
@@ -95,7 +95,43 @@ def bool_crack(datas):
                     data4 += ' '
         except:
             continue
-    output = [data1.lstrip(),data2.lstrip(),data3.lstrip(),data4.lstrip()]
+    data5,data6,IS_LEN,IS_SET,LENS = '','',False,False,[]
+    for i in range(len(datas)):
+        LEN = datas[i].split('LENGTH')
+        if(len(LEN) != 1):
+            IS_LEN = True
+            LENS.append(LEN[0])
+    LENS = list(set(LENS))
+    if(len(LENS) == 2):
+        IS_SET = True
+    if(IS_SET and IS_LEN):
+        for i in range(len(datas) - 1):
+            try:
+                chat = re.search(r'\),(\d+),', datas[i])
+                next_chat = re.search(r'\),(\d+),', datas[i + 1])
+                if (chat):
+                    chat = chat.group(1)
+                if (next_chat):
+                    next_chat = next_chat.group(1)
+                if (chat != next_chat):
+                    length = datas[i].split('LENGTH')[0]
+                    data = re.search(r'\)(>|<|=)(.*?)(\)|\)\)|--+|#|HTTP)', datas[i])
+                    if (data):
+                        data = data.group(2).replace("'", "").replace('"', "")
+                        if (data.isdigit() and int(data) > 32):
+                            try:
+                                if(length == LENS[0]):
+                                    data5 += chr(int(data))
+                                    data6 += chr(int(data)+1)
+                                else:
+                                    data5 += chr(int(data)+1)
+                                    data6 += chr(int(data))
+                            except:
+                                pass
+            except:
+                continue
+
+    output = [data1.lstrip(),data2.lstrip(),data3.lstrip(),data4.lstrip(),data5.lstrip(),data6.lstrip()]
     return output
 
 def mains(pcap_file):
